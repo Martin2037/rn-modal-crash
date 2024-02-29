021c1ca3eb5bff980f94280c788f0da1dd85aa89
zhujunyang🌝 <zhujunyang@futurevista.work>
2024-02-29 17:30:47 +0800

    :bug: crash

--- App.jsx
+++ App.jsx
@@ -1,118 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import '@walletconnect/react-native-compat'
+import {sepolia, WagmiConfig} from 'wagmi'
+import {mainnet, polygon, arbitrum, optimism, bsc} from 'viem/chains'
+import { createWeb3Modal, defaultWagmiConfig, Web3Modal,W3mButton } from '@web3modal/wagmi-react-native'
+import {Text, View} from 'react-native'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = 'PROJECT_ID'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+// 2. Create config
+const metadata = {
+    name: 'Tap3',
+    description: 'Tap3',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886'],
+    redirect: {
+        native: Platform.OS === 'android' ? 'tap3://test/' : 'tap3://',
+        universal: Platform.OS === 'android' ? 'tap3://test/' : 'tap3://',
+    },
+};
+const chains = [mainnet, polygon, arbitrum, bsc, optimism, sepolia];
+const wagmiConfig = defaultWagmiConfig({chains, projectId, metadata});
+createWeb3Modal({
+    projectId,
+    chains,
+    wagmiConfig,
+    enableAnalytics: true // Optional - defaults to your Cloud configuration
 });
 
-export default App;
+export default function App() {
+  return (
+      <WagmiConfig config={wagmiConfig}>
+        <Text>11</Text>
+        <View style={{
+            marginTop: 100
+        }}>
+            <W3mButton />
+        </View>
+        <Web3Modal />
+      </WagmiConfig>
+  )
+}
